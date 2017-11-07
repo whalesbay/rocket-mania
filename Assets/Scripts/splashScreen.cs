12e4b05993e6483816266379cafe5f3438da1a29
xupertheo <xupertheo@hotmail.com>
2017-11-07 16:47:57 +0100

    Added script and dwsign to the splahScreen

---
+++ Assets/Scripts/splashScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class splashScreen : MonoBehaviour {
+
+	public string sceneToLoad;
+
+	public int secTillSceneLoad;
+
+	// Use this for initialization
+	void Start () {
+
+		Invoke("OpenNextScene", secTillSceneLoad);
+
+	}
+
+	void OpenNextScene() {
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
